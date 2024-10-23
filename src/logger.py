a8e6e292a3c4a077dbaf02dc87a57554a953a57d
Arun_Sai <arun.sai.thunga@fau.de>
2024-10-23 20:04:18 +0200

    data ingestion

--- src/logger.py
+++ src/logger.py
@@ -4,8 +4,17 @@ from datetime import datetime
 
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path= os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Get the path one directory above the current working directory.
+logs_path = os.path.join(root_dir,"..", "logs")
+
+# Ensure the logs directory exists.
+os.makedirs(logs_path, exist_ok=True)
+
+# Create the full path for the log file.
+log_file_path = os.path.join(logs_path, LOG_FILE)
 
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
