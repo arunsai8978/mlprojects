6fb3e6dac52d6f2bd606a644332583fae29109ca
Arun_Sai <arun.sai.thunga@fau.de>
2024-10-24 12:20:34 +0200

    hyper parameters added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -51,16 +51,16 @@ class DataIngestion:
             raise CustomExpection(e, sys)
 
 
-# if __name__==  "__main__":
-#     obj = DataIngestion()
-#     train_data, test_data = obj.initiate_data_ingestion()
-#
-#     data_transformation = DataTransformation()
-#
-#     train_arr, test_arr,_ =data_transformation.initiate_data_tranformation(train_data,test_data)
-#
-#     modeltrainer= ModelTrainer()
-#     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+if __name__==  "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+
+    train_arr, test_arr,_ =data_transformation.initiate_data_tranformation(train_data,test_data)
+
+    modeltrainer= ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
 
