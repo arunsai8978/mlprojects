a0dd3130c1e691ad3c0b3232eadc156c3e1976e2
Arun_Sai <arun.sai.thunga@fau.de>
2024-10-25 10:01:02 +0200

    updated app

--- src/utils.py
+++ src/utils.py
@@ -40,10 +40,10 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,param):
             model.fit(X_train, y_train)  # Train model
 
             # Make predictions
-            y_train_pred = model.predict(X_train)
+            # y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
-            train_model_score = r2_score(y_train,y_train_pred)
+            # train_model_score = r2_score(y_train,y_train_pred)
             test_model_score = r2_score(y_test,y_test_pred)
 
             report[list(models.keys())[i]]=test_model_score
